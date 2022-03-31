5f839ed1f035fc596505957af0af3d853633489f
l-gu <laurent@telosys.org>
2022-04-01 00:24:26 +0200

    Initial commit

---
+++ orders-microservice/src/main/java/org/demo/orders/SpringBootApp.java
@@ -0,0 +1,21 @@
+package org.demo.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBootApp {
+
+	/**
+	 * Entry point for spring-boot based app
+	 *
+	 * @param args
+	 *            - arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+
+}
